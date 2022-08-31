fc237c61e328c893c66fb107df4d0eced1f8650b
KatayoonGoshvadi <katayoon.goshvadi+github@gmail.com>
2022-08-31 17:38:21 +0000

    formatted

--- dmcx/sampler/randomwalk.py
+++ dmcx/sampler/randomwalk.py
@@ -20,7 +20,7 @@ class RandomWalkSampler(abstractsampler.AbstractSampler):
       index_axis1 = random.permutation(
           rnd, aranged_index_batch, axis=-1,
           independent=True)[:, 0:num_flips].reshape(1, -1)
-      index_axis0 = jnp.array([jnp.arange(x.shape[0])] * num_flips).flatten('F')
+      index_axis0 = jnp.array([jnp.arange(x.shape[0])] * num_flips).flatten("F")
       zeros = jnp.zeros(x.shape)
       # 2d array with 1 in flipped indices
       flipped = zeros.at[index_axis0, index_axis1].set(1)
@@ -37,22 +37,23 @@ class RandomWalkSampler(abstractsampler.AbstractSampler):
 
     def is_accepted(rnd, accept_ratio):
       u = random.uniform(rnd, shape=accept_ratio.shape, minval=0.0, maxval=1.0)
-      return jnp.expand_dims(jnp.where(accept_ratio <= u, 1, 0), axis= -1)
+      return jnp.expand_dims(jnp.where(accept_ratio <= u, 1, 0), axis=-1)
 
     def update_state(accept_ratio, u, x):
-      return min( max([1, u + ( jnp.mean(accept_ratio) - self.target_accept_ratio)]), x.shape[-1] )
+      return min(
+          max([1, u + (jnp.mean(accept_ratio) - self.target_accept_ratio)]),
+          x.shape[-1])
 
     print("XXXXXXXXXXXXXXXXX")
     print(x)
     rnd_uniform, rnd_new_sample, rnd_acceptance = random.split(rnd, num=3)
     del rnd
     u = state
-    
+
     if random.uniform(rnd_uniform, minval=0.0, maxval=1.0) < u - jnp.floor(u):
       num_flips = int(jnp.floor(u))
     else:
       num_flips = int(jnp.ceil(u))
-
 
     new_x = get_new_sample(rnd_new_sample, x, num_flips)
     print("newwwwwwXXXXXXXXXXXXXXXXX")
@@ -63,16 +64,15 @@ class RandomWalkSampler(abstractsampler.AbstractSampler):
     accepted = is_accepted(rnd_acceptance, accept_ratio)
     print("accepted")
     print(accepted)
-    
+
     print(jnp.shape(x), jnp.shape(new_x), jnp.shape(accepted))
     new_x = accepted * new_x + (1 - accepted) * x
-    
+
     print("returned x")
     print(new_x)
 
     new_state = state
     if self.adaptive:
       new_state = update_state(accepted, u, x)
-    
+
     return new_x, new_state
-  
