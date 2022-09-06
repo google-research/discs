5040aaeeb49d3dc1c4c1333a9329162138d004c4
Kati Goshvadi <kgoshvadi@google.com>
2022-09-06 17:37:37 +0000

    Added pmap and Var,Mean Calculation

--- dmcx/main/main.py
+++ dmcx/main/main.py
@@ -4,6 +4,9 @@ from collections.abc import Sequence
 from absl import app
 import dmcx.model.bernouli as bernouli_model
 import dmcx.sampler.randomwalk as randomwalk_sampler
+import os
+
+os.environ['XLA_FLAGS'] = '--xla_force_host_platform_device_count=1'
 import jax
 from jax import random
 import jax.numpy as jnp
@@ -15,15 +18,22 @@ def load_configs():
 
   config_main = config_dict.ConfigDict(
       initial_dictionary=dict(
-          model='ber', sampler='RW', num_samples=5, chain_lenght=2))
+          parallel=True,
+          model='ber',
+          sampler='RW',
+          num_samples=100,
+          chain_lenght=5000,
+          chain_burn_in=4500))
   config_model = config_dict.ConfigDict(
-      initial_dictionary=dict(dimension=10, init_sigma=1.0))
+      initial_dictionary=dict(dimension=5, init_sigma=1.0))
   config_sampler = config_dict.ConfigDict(
       initial_dictionary=dict(
           adaptive=False,
           target_accept_ratio=0.234,
-          sample_dimension=10,
+          sample_dimension=5,
           num_categ=2))
+  if config_model.dimension != config_sampler.sample_dimension:
+    config_model.dimension = config_sampler.sample_dimension
   return config_main, config_model, config_sampler
 
 
@@ -37,6 +47,36 @@ def get_sampler(config_main, config_sampler):
   if config_main.sampler == 'RW':
     return randomwalk_sampler.RandomWalkSampler(config_sampler)
   raise Exception('Please provide a correct sampler name.')
+
+
+def get_sample_mean(samples):
+  mean_over_samples = jnp.mean(samples, axis=1)
+  mean_over_batch = jnp.mean(mean_over_samples, axis=0)
+  return mean_over_batch
+
+
+def get_sample_variance(samples):
+  var_over_samples = jnp.var(samples, axis=1)
+  mean_var_over_batch = jnp.mean(var_over_samples, axis=0)
+  return mean_var_over_batch
+
+
+def compute_chain(model, chain_lenght, chain_burn_in, step_jit, state, params,
+                  rng_sampler_step, x):
+  chain = []
+  chain.append(x)
+  for i in range(chain_lenght - 1):
+    x, state = step_jit(model, rng_sampler_step, x, params, state)
+    rng_sampler_step, _ = random.split(rng_sampler_step)
+    if chain_burn_in <= i:
+      chain.append(x)
+  chain = jnp.swapaxes(jnp.array(chain), axis1=0, axis2=1)
+  return chain
+
+
+def split(arr, n_devices):
+  """Splits the first axis of `arr` evenly across the number of devices."""
+  return arr.reshape(n_devices, arr.shape[0] // n_devices, *arr.shape[1:])
 
 
 def main(argv: Sequence[str]) -> None:
@@ -55,14 +95,31 @@ def main(argv: Sequence[str]) -> None:
   x = model.get_init_samples(rng_x0, config_main.num_samples)
   state = sampler.make_init_state(rng_sampler)
   step_jit = jax.jit(sampler.step, static_argnums=0)
-  chain = []
-  chain.append(x)
-  for _ in range(config_main.chain_lenght - 1):
-    x, state = step_jit(model, rng_sampler_step, x, params, state)
-    rng_sampler_step, _ = random.split(rng_sampler_step)
-    chain.append(x)
-  chain = jnp.swapaxes(jnp.array(chain), axis1=0, axis2=1)
-  print(jnp.shape(chain))
+  n_devices = jax.local_device_count()
+
+  if not config_main.parallel:
+    chain = compute_chain(model, config_main.chain_lenght,
+                          config_main.chain_burn_in, step_jit, state, params,
+                          rng_sampler_step, x)
+  else:
+    params = jnp.stack([params] * n_devices)
+    rng_sampler_step = jax.random.split(rng_sampler_step, num=n_devices)
+    x = split(x, n_devices)
+    print('Num devices: ', n_devices, ',X shape: ', x.shape, ',Params shape: ',
+          params.shape, ',Random Key shape: ', rng_sampler_step.shape)
+    compute_chain_p = jax.pmap(
+        compute_chain, static_broadcasted_argnums=[0, 1, 2, 3, 4])
+    chain = compute_chain_p(model, config_main.chain_lenght,
+                            config_main.chain_burn_in, step_jit, state, params,
+                            rng_sampler_step, x)
+    chain = chain.reshape(x.shape[0], -1, x.shape[-1])
+  print('Samples Shape=', jnp.shape(chain))
+  mean = get_sample_mean(chain)
+  var = get_sample_variance(chain)
+  print('Sample Mean: ', mean)
+  print('Population Mean: ', model.get_expected_val(params))
+  print('Sample Var: ', var)
+  print('Population Var: ', model.get_var(params))
 
 
 if __name__ == '__main__':
