bd854a44801ebac4d02cb8f0d5086652323e3f54
KatayoonGoshvadi <katayoon.goshvadi+github@gmail.com>
2022-10-19 15:56:20 +0000

    binary working

---
+++ dmcx/model/categorical.py
@@ -0,0 +1,66 @@
+"""Categorical Factorized Energy Function."""
+
+from dmcx.model import abstractmodel
+import jax
+import jax.numpy as jnp
+import ml_collections
+import pdb
+
+
+class Categorical(abstractmodel.AbstractModel):
+  """Categorical Distribution."""
+
+  def __init__(self, config: ml_collections.ConfigDict):
+
+    self.shape = config.shape
+    self.init_sigma = config.init_sigma
+    self.num_categories = config.num_categories
+    self.one_hot_representation = config.one_hot_representation
+
+  def make_init_params(self, rnd):
+    params = jax.random.normal(
+        rnd, shape=(self.shape + (self.num_categories,))) * self.init_sigma
+    return params
+
+  def get_one_hot_represntation(self, x0):
+    x0 = jnp.expand_dims(x0, axis=-1)
+    x0_one_hot = jnp.tile(jnp.arange(self.num_categories), x0.shape)
+    return jnp.array(x0 == x0_one_hot, dtype=jnp.int32)
+
+  def get_init_samples(self, rnd, num_samples: int):
+
+    x0 = jax.random.randint(
+        rnd,
+        shape=(num_samples,) + self.shape,
+        minval=0,
+        maxval=self.num_categories,
+        dtype=jnp.int32)
+
+    if self.one_hot_representation:
+      return self.get_one_hot_represntation(x0)
+
+    return x0
+
+  def forward(self, params, x):
+    # x not following one-hot representation
+    if len(x.shape) == (len(self.shape)+1):
+      x = self.get_one_hot_represntation(x)
+
+    params = jnp.expand_dims(params, axis=0)
+    loglikelihood_over_categories = jnp.sum((x * params), axis=-1)
+    loglikelihood = jnp.sum(
+        loglikelihood_over_categories.reshape(x.shape[0], -1), axis=-1)
+    return loglikelihood
+
+  def get_value_and_grad(self, params, x):
+    
+    if not self.one_hot_representation:
+      x = self.get_one_hot_represntation(x)
+    x = x.astype(jnp.float32)  # int tensor is not differentiable
+
+    def fun(z):
+      loglikelihood = self.forward(params, z)
+      return jnp.sum(loglikelihood), loglikelihood
+
+    (_, loglikelihood), grad = jax.value_and_grad(fun, has_aux=True)(x)
+    return loglikelihood, grad
--- dmcx/sampler/gibbswithgrad.py
+++ dmcx/sampler/gibbswithgrad.py
@@ -7,15 +7,24 @@ import ml_collections
 import math
 import pdb
 
+# [[1 0 0][0 1 0]]
+# pdb.set_trace()
+# score_change_x = grad - (grad * x).sum(dim=-1, keepdim=True)
+# score_change_x = score_change_x - 1e9 * x
+# dist_x = StableOnehotCategorical(logits=score_change_x.view(bsize, -1))
+# index_x = dist_x.sample()
+# log_x2y = dist_x.log_prob(index_x)
+# index_y = x * index_x.view(x.shape).sum(dim=-1, keepdim=True)
+# y = x * (1 - index_x.view(x.shape).sum(dim=-1, keepdim=True)) + index_x.view(x.shape)
+# return x
+
 
 class GibbsWithGradSampler(abstractsampler.AbstractSampler):
   """Gibbs With Grad Sampler Class."""
 
   def __init__(self, config: ml_collections.ConfigDict):
-    if isinstance(config.sample_shape, int):
-      self.sample_shape = (config.sample_shape,)
-    else:
-      self.sample_shape = config.sample_shape
+
+    self.sample_shape = config.sample_shape
     self.num_categories = config.num_categories
 
   def make_init_state(self, rnd):
@@ -37,32 +46,38 @@ class GibbsWithGradSampler(abstractsampler.AbstractSampler):
       New sample.
     """
 
-    def compute_delta_x(x, model, model_param):
+    def compute_loglike_delta(x, model, model_param):
       _, grad = model.get_value_and_grad(model_param, x)
       if self.num_categories == 2:
-        return (-2 * x + 1) * grad
+        return (-2 * x + 1) * grad  #shape of sample
       else:
-        pdb.set_trace()
-        score_change_x = grad - (grad * x).sum(dim=-1, keepdim=True)
-        score_change_x = score_change_x - 1e9 * x
-        
-        # dist_x = StableOnehotCategorical(logits=score_change_x.view(bsize, -1))
-        # index_x = dist_x.sample()
-        # log_x2y = dist_x.log_prob(index_x)
-        # index_y = x * index_x.view(x.shape).sum(dim=-1, keepdim=True)
-        # y = x * (1 - index_x.view(x.shape).sum(dim=-1, keepdim=True)) + index_x.view(x.shape)
-        
-        return x
-        
-
-    def compute_index_dist(x, model, model_param):
-      delta_x = compute_delta_x(x, model, model_param)
-      delta_x_flatten = (delta_x / 2).reshape(delta_x.shape[0], -1)
-      return jnp.exp(delta_x_flatten) / jnp.sum(
-          jnp.exp(delta_x_flatten), axis=1, keepdims=True)
+        return (jnp.ones(x.shape) - x) * grad  #shape of sample with categories
 
     def select_index(rnd_categorical, loglikelihood):
-      return random.categorical(rnd_categorical, loglikelihood, axis=1)
+      
+      if self.num_categories == 2:
+        loglikelihood_flatten = loglikelihood.reshape(loglikelihood.shape[0],
+                                                      -1)
+        print(jnp.shape(loglikelihood_flatten))
+        return random.categorical(
+            rnd_categorical, loglikelihood_flatten, axis=1)
+      else:
+        rnd_index, rnd_category = random.split(rnd_categorical)
+        loglikelihood_over_categories = loglikelihood
+        # sum over categories
+        loglikelihood = jnp.sum(loglikelihood, axis=-1)
+        loglikelihood_flatten = loglikelihood.reshape(loglikelihood.shape[0],
+                                                      -1)
+        selected_index_flatten = random.categorical(
+            rnd_index, loglikelihood_flatten, axis=1)
+        dim = math.prod(self.sample_shape)
+        loglikelihood_over_categories_flatten = loglikelihood_over_categories.reshape(
+            loglikelihood.shape[0], dim, self.num_categories)
+        loglikelihood_categories = loglikelihood_over_categories_flatten[
+            jnp.arange(loglikelihood.shape[0]), selected_index_flatten]
+        selected_category = random.categorical(
+            rnd_category, loglikelihood_categories, axis=1)
+        return jnp.array([selected_index_flatten, selected_category])
 
     def generate_new_samples(rnd, x, model, model_param):
       """Generate the new samples, given the current samples based on the given expected hamming distance.
@@ -76,20 +91,36 @@ class GibbsWithGradSampler(abstractsampler.AbstractSampler):
       Returns:
         New samples.
       """
-      dist_flatten = compute_index_dist(x, model, model_param)
-      select_i_flatten = select_index(rnd, dist_flatten)
 
-      x_flatten = x.reshape(x.shape[0], -1)
+      pdb.set_trace()
+
+      loglike_delta_x = compute_loglike_delta(x, model, model_param) / 2
+      selected_index_flatten = select_index(rnd, loglike_delta_x)
 
       if self.num_categories == 2:
-        new_vals = (x_flatten[jnp.arange(x.shape[0]), select_i_flatten] + 1) % 2
-        new_x = x_flatten.at[jnp.arange(x.shape[0]),
-                             select_i_flatten].set(new_vals)
-        new_x = new_x.reshape((x.shape[0],) + self.sample_shape)
+        flipped = jnp.zeros(x.shape)
+        flipped_flatten = flipped.reshape(x.shape[0], -1)
+        flipped_flatten = flipped_flatten.at[jnp.arange(x.shape[0]),
+                                             selected_index_flatten].set(
+                                                 jnp.ones(x.shape[0]))
+        flipped = flipped_flatten.reshape(x.shape)
+        y = (x + flipped) % self.num_categories
+      else:
+        selected_index = selected_index_flatten[0]
+        selected_category = selected_index_flatten[1]
+        new_x = jnp.zeros([x.shape[0], self.num_categories] )
+        new_x = new_x.at[jnp.arange(x.shape[0]), selected_category].set(jnp.ones(x.shape[0]))
+        dim = math.prod(self.sample_shape)
+        y_flatten = x.reshape(x.shape[0], dim, self.num_categories)
+        y_flatten = y_flatten.at[jnp.arange(x.shape[0]), selected_index].set(new_x)
+        y = y_flatten.reshape(x.shape)
 
-      return new_x, select_i_flatten
+      return y, selected_index_flatten
 
     def select_new_samples(rnd_acceptance, model, model_param, x, y, i_flatten):
+      
+      pdb.set_trace()
+      
       accept_ratio = get_ratio(model, model_param, x, y, i_flatten)
       accepted = is_accepted(rnd_acceptance, accept_ratio)
       accepted = accepted.reshape(accepted.shape +
@@ -97,24 +128,54 @@ class GibbsWithGradSampler(abstractsampler.AbstractSampler):
       new_x = accepted * y + (1 - accepted) * x
       return new_x
 
+    def compute_softmax(loglikelihood):
+      # pdb.set_trace()
+      
+      loglikelihood = (loglikelihood).reshape(loglikelihood.shape[0], -1)
+      return jnp.exp(loglikelihood) / jnp.sum(
+          jnp.exp(loglikelihood), axis=1, keepdims=True)
+
+    def compute_probab_index(loglikelihood, i_flatten):
+
+      pdb.set_trace()
+      if self.num_categories == 2:
+        probability = compute_softmax(loglikelihood)
+        return probability[jnp.arange(loglikelihood.shape[0]), i_flatten]
+      else:
+        loglikelihood_category = loglikelihood[jnp.arange(loglikelihood.shape[0]), i_flatten]
+        probability = compute_softmax(loglikelihood_category)
+        return jnp.take(probability, i_flatten)
+      
     def get_ratio(model, model_param, x, y, i_flatten):
+      
+      pdb.set_trace()
+      
+
+      loglike_delta_x = compute_loglike_delta(x, model, model_param)
+      loglike_delta_y = compute_loglike_delta(y, model, model_param)
+
+      probab_i_given_x = compute_probab_index(loglike_delta_x, i_flatten)
+      probab_i_given_y = compute_probab_index(loglike_delta_y, i_flatten)
 
       loglikelihood_x = model.forward(model_param, x)
       loglikelihood_y = model.forward(model_param, y)
-      q_i_x = compute_index_dist(x, model, model_param).reshape(x.shape[0], -1)
-      q_i_y = compute_index_dist(y, model, model_param).reshape(x.shape[0], -1)
-      dist_index_ratio = (q_i_y / q_i_x)[jnp.arange(x.shape[0]), i_flatten]
-      return jnp.exp(loglikelihood_y - loglikelihood_x) * (dist_index_ratio)
+
+      return jnp.exp(loglikelihood_y - loglikelihood_x) * (
+          probab_i_given_y / probab_i_given_x)
 
     def is_accepted(rnd_acceptance, accept_ratio):
+      
+      pdb.set_trace()
+      
       random_uniform_val = random.uniform(
           rnd_acceptance, shape=accept_ratio.shape, minval=0.0, maxval=1.0)
       return jnp.where(accept_ratio >= random_uniform_val, 1, 0)
 
+
     rnd_new_sample, rnd_acceptance = random.split(rnd)
     del rnd
     y, i_flatten = generate_new_samples(rnd_new_sample, x, model, model_param)
-
+    pdb.set_trace()
     new_x = select_new_samples(rnd_acceptance, model, model_param, x, y,
                                i_flatten)
     new_state = state
