e618301dcb6f9f830e00c2e67f50ed678a40d6ee
riemannnn@gmail.com <riemannnn@gmail.com>
2022-12-07 11:36:20 -0800

    add log1mexp

--- dmcx/common/utils.py
+++ dmcx/common/utils.py
@@ -1,6 +1,7 @@
 """Utilities."""
 
 import jax
+import jax.numpy as jnp
 
 
 class RngGen(object):
@@ -19,3 +20,9 @@ class RngGen(object):
   def advance(self, count: int):
     self._counter += count
     return jax.random.fold_in(self._base_rng, self._counter)
+
+def log1mexp(x):
+  # Computes log(1-exp(-|x|))
+  # See https://cran.r-project.org/web/packages/Rmpfr/vignettes/log1mexp-note.pdf
+  x = -jnp.abs(x)
+  return jnp.where(x > -0.693, jnp.log(-jnp.expm1(x)), jnp.log1p(-jnp.exp(x)))
