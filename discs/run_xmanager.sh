9fcdcf97df0815825626b31bd2a40d21b633d4b5
KatayoonGoshvadi <katayoon.goshvadi+github@gmail.com>
2022-12-13 23:45:32 +0000

    Removed comments

--- discs/run_xmanager.sh
+++ discs/run_xmanager.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
 
-# model=bernoulli
-# sampler=randomwalk
-# xmanager launch discs/xm_launcher.py -- \
-#     --model_config="discs/models/configs/${model?}_config.py" \
-#     --sampler_config="discs/samplers/configs/${sampler?}_config.py" \
-#     --xm_resource_alloc="user:xcloud/${USER}" \
-#     --experiment_name="Sampling_Experiment_${sampler?}-${model?}" \
-
 models="bernoulli ising"
 samplers="randomwalk gibbswithgard"
 
