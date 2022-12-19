4d546e34f69fc19f6513d2945f17c2e46d25e8b8
KatayoonGoshvadi <katayoon.goshvadi+github@gmail.com>
2022-12-19 19:11:39 +0000

    Developed adaptive gibbs with grad

--- discs/samplers/configs/gibbswithgrad_config.py
+++ discs/samplers/configs/gibbswithgrad_config.py
@@ -4,5 +4,7 @@ from ml_collections import config_dict
 def get_config():
   model_config = dict(
       name='gibbswithgrad',
+      adaptive=True,
+      target_acceptance_rate=0.574,
   )
   return config_dict.ConfigDict(model_config)
