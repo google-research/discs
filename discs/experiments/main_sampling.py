964b07ddcc8eb51a05bf4e2e9d61f86016a463c4
KatayoonGoshvadi <katayoon.goshvadi+github@gmail.com>
2022-12-21 00:39:05 +0000

    Saving the results into csv file

--- discs/experiments/main_sampling.py
+++ discs/experiments/main_sampling.py
@@ -8,8 +8,11 @@ from ml_collections import config_flags
 from discs.common import configs as common_configs
 from discs.experiment import experiment as experiment_mod
 from discs.evaluation import evaluator as evaluator_mod
+import numpy as np
 import time
 import os
+import pdb
+import csv
 
 _MODEL_CONFIG = config_flags.DEFINE_config_file('model_config')
 _SAMPLER_CONFIG = config_flags.DEFINE_config_file('sampler_config')
@@ -47,6 +50,49 @@ def main(_):
 
   if not os.path.isdir(_SAVE_DIR.value):
     os.makedirs(_SAVE_DIR.value)
+
+  results = {}
+
+  results['sampler'] = config.sampler.name
+  if 'adaptive' in config.sampler.keys():
+    results['sampler'] = f'a_{config.sampler.name}'
+
+  if 'balancing_fn_type' in config.sampler.keys():
+    if config.sampler.balancing_fn_type == 2:
+      results['sampler'] = results['sampler'] + '(frac)'
+    elif config.sampler.balancing_fn_type == 3:
+      results['sampler'] = results['sampler'] + '(and)'
+    elif config.sampler.balancing_fn_type == 4:
+      results['sampler'] = results['sampler'] + '(or)'
+    else:
+      results['sampler'] = results['sampler'] + '(sqrt)'
+
+  ess_metrcis = np.array(ess_metrcis)
+  results['model'] = config.model.name
+  results['num_categories'] = config.model.num_categories
+  results['shape'] = config.model.shape
+  results['ESS'] = ess_metrcis[0]
+  results['ESS_M-H'] = ess_metrcis[1]
+  results['ESS_T'] = ess_metrcis[2]
+  results['ESS_EE'] = ess_metrcis[3]
+  results['Time'] = running_time
+  results['batch_size'] = config.experiment.batch_size
+  results['chain_length'] = config.experiment.chain_length
+  results['ess_ratio'] = config.experiment.ess_ratio
+
+
+  csv_path = f'{_SAVE_DIR.value}/results.csv'
+  if not os.path.exists(csv_path):
+    with open(f'{_SAVE_DIR.value}/results.csv', 'w') as csvfile:
+      writer = csv.DictWriter(csvfile, fieldnames=list(results.keys()))
+      writer.writeheader()
+      writer.writerow(results)
+      csvfile.close()
+  else:
+    with open(f'{_SAVE_DIR.value}/results.csv', 'a') as csvfile:
+      writer = csv.DictWriter(csvfile, fieldnames=list(results.keys()))
+      writer.writerow(results)
+      csvfile.close()
 
   with open(
       f'{_SAVE_DIR.value}/{config.model.name}_{config.sampler.name}_{running_time}.txt',
