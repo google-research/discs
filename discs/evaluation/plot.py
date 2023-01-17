3697a660018490107be7c0cf515db98dc839e6b3
KatayoonGoshvadi <katayoon.goshvadi+github@gmail.com>
2023-01-17 18:59:37 +0000

    plotting loops through result folder

--- discs/evaluation/plot.py
+++ discs/evaluation/plot.py
@@ -2,46 +2,54 @@ import csv
 import numpy as np
 import matplotlib.pyplot as plt
 import pdb
+import os
 
-# open the file in read mode
-filename = open('./results.csv', 'r')
- 
-# creating dictreader object
-file = csv.DictReader(filename)
+results_dir = './discs/'
+all_dirs = os.listdir(results_dir)
 
-samplers = []
-ess_ee = []
-ess_clock = []
-for col in file:
-  model = col['model']
-  samplers.append( col['sampler'])
-  ess_ee.append( float(col['ESS_EE'])*50000 )
-  ess_clock.append(float(col['ESS_T']) )
+pdb.set_trace()
+for curr_dir in all_dirs:
+    if curr_dir.startswith('results_'):
+      curr_path = os.path.join(results_dir, curr_dir)
+      # open the file in read mode
+      filename = open(curr_path+'/results.csv', 'r')
+      
+      # creating dictreader object
+      file = csv.DictReader(filename)
 
-ess_ee = np.sort(np.array(ess_ee))
-ess_clock = np.sort(np.array(ess_clock))
-x_pos = 0.3 * np.arange(len(samplers))
-c = ['green', 'red', 'red', 'saddlebrown', 'saddlebrown', 'pink', 'pink'][0:len(x_pos)]
-fig = plt.figure(figsize = (10, 6))
-plt.yscale('log')
-plt.bar(x_pos, ess_ee, width = 0.2, color=c)
-plt.xticks(x_pos, samplers)
+      samplers = []
+      ess_ee = []
+      ess_clock = []
+      for col in file:
+        model = col['model']
+        samplers.append( col['sampler'])
+        ess_ee.append( float(col['ESS_EE'])*50000 )
+        ess_clock.append(float(col['ESS_T']) )
 
-plt.xlabel("Samplers")
-plt.ylabel("ESS (high temp)")
-plt.title(f'ESS w.r.t. Energy Evaluations on {model}')
-plt.show()
-plt.savefig('EssEE.png')
+      ess_ee = np.sort(np.array(ess_ee))
+      ess_clock = np.sort(np.array(ess_clock))
+      x_pos = 0.3 * np.arange(len(samplers))
+      c = ['green', 'red', 'red', 'saddlebrown', 'saddlebrown', 'pink', 'pink'][0:len(x_pos)]
+      fig = plt.figure(figsize = (10, 6))
+      plt.yscale('log')
+      plt.bar(x_pos, ess_ee, width = 0.2, color=c)
+      plt.xticks(x_pos, samplers)
 
-#########
+      plt.xlabel("Samplers")
+      plt.ylabel("ESS (high temp)")
+      plt.title(f'ESS w.r.t. Energy Evaluations on {model}')
+      plt.show()
+      plt.savefig('EssEE.png')
 
-fig = plt.figure(figsize = (10, 6))
-plt.yscale('log')
-plt.bar(x_pos, ess_clock, width = 0.2, color=c)
-plt.xticks(x_pos, samplers)
- 
-plt.xlabel("Samplers")
-plt.ylabel("ESS (high temp)")
-plt.title(f'ESS w.r.t. Wall Clock Time on {model}')
-plt.show()
-plt.savefig('EssClock.png')
+      #########
+
+      fig = plt.figure(figsize = (10, 6))
+      plt.yscale('log')
+      plt.bar(x_pos, ess_clock, width = 0.2, color=c)
+      plt.xticks(x_pos, samplers)
+      
+      plt.xlabel("Samplers")
+      plt.ylabel("ESS (high temp)")
+      plt.title(f'ESS w.r.t. Wall Clock Time on {model}')
+      plt.show()
+      plt.savefig('EssClock.png')
