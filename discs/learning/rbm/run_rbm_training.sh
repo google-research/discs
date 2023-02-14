ed642f9cc527d92809b47de98a15370804482538
KatayoonGoshvadi <katayoon.goshvadi+github@gmail.com>
2023-02-14 21:59:24 +0000

    cleaned the script

--- discs/learning/rbm/run_rbm_training.sh
+++ discs/learning/rbm/run_rbm_training.sh
@@ -8,6 +8,6 @@ export CUDA_VISIBLE_DEVICES=0,1
 export XLA_FLAGS="--xla_force_host_platform_device_count=2"
 
 python -m discs.learning.rbm.main_rbm_training \
-  --config="discs/learning/rbm/exp_config.py:${dataset}-${num_categories}-${num_hidden}" \
+  --config="discs/learning/rbm/exp_config.py" \
   --save_root=$save_root \
   $@
