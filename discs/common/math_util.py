f91bcbd5e1a9bf7f02f835233455bb340c236a45
KatayoonGoshvadi <katayoon.goshvadi+github@gmail.com>
2023-05-05 18:13:01 +0000

    u for binary case

--- discs/common/math_util.py
+++ discs/common/math_util.py
@@ -5,9 +5,8 @@ import jax
 import jax.numpy as jnp
 
 
-
 def comb(x, y):
-  return math.factorial(x)/(math.factorial(y) * math.factorial(x-y))
+  return int(math.factorial(x) / (math.factorial(y) * math.factorial(x - y)))
 
 
 def prod(it):
@@ -25,7 +24,7 @@ def gumbel(rng, loc):
   uniform_sample = jax.random.uniform(
       rng, shape=loc.shape, minval=0.0, maxval=1.0
   )
-  return loc -jnp.log(-jnp.log(uniform_sample))
+  return loc - jnp.log(-jnp.log(uniform_sample))
 
 
 def log1mexp(x):
@@ -43,9 +42,16 @@ def noreplacement_sampling_renormalize(ll_idx, axis=-1):
   return ll_idx
 
 
-def multinomial(rng, log_prob, num_samples, replacement=True,
-                is_nsample_const=True, batch_size=1, return_ll=False,
-                need_ordering_info=False):
+def multinomial(
+    rng,
+    log_prob,
+    num_samples,
+    replacement=True,
+    is_nsample_const=True,
+    batch_size=1,
+    return_ll=False,
+    need_ordering_info=False,
+):
   """Multinomial sample.
 
   Args:
@@ -57,7 +63,8 @@ def multinomial(rng, log_prob, num_samples, replacement=True,
     batch_size: only used when replacement=True and is_nsample_const=False
     return_ll: return log-likelihood of selected indices?
     need_ordering_info: when is_nsample_const=False and replacement=False,
-    whether we need the ordered sample information or not.
+      whether we need the ordered sample information or not.
+
   Returns:
     sampled indices: in the form of either a counting matrix (
     is_nsample_const=False), or an index matrix (is_nsample_const=True)
@@ -67,8 +74,9 @@ def multinomial(rng, log_prob, num_samples, replacement=True,
   pbatch_shape = log_prob.shape[:-1]
   if is_nsample_const:
     if replacement:
-      idx = jax.random.categorical(key=rng, logits=log_prob,
-                                   shape=(num_samples,) + pbatch_shape)
+      idx = jax.random.categorical(
+          key=rng, logits=log_prob, shape=(num_samples,) + pbatch_shape
+      )
       idx = jnp.transpose(idx, axes=tuple(range(1, idx.ndim)) + (0,))
     else:
       perturbed_ll = gumbel(rng, loc=log_prob)
@@ -82,32 +90,38 @@ def multinomial(rng, log_prob, num_samples, replacement=True,
       return idx
   else:
     if replacement:
+
       def body_fun(val):
         cnt, key, total_count = val
         key, next_key = jax.random.split(key)
         idx = jax.random.categorical(
-            key=key, logits=log_prob, shape=(batch_size,) + pbatch_shape)
+            key=key, logits=log_prob, shape=(batch_size,) + pbatch_shape
+        )
         onehot = jax.nn.one_hot(idx, num_classes=num_classes, dtype=jnp.int32)
         mask = jnp.arange(batch_size, dtype=cnt.dtype) + cnt < num_samples
         mask = jnp.expand_dims(mask, range(1, onehot.ndim)).astype(jnp.int32)
         onehot = onehot * mask
         total_count = total_count + jnp.sum(onehot, axis=0)
         return (cnt + batch_size, next_key, total_count)
-      init_val = (jnp.zeros(shape=(), dtype=num_samples.dtype),
-                  rng,
-                  jnp.zeros(shape=log_prob.shape, dtype=jnp.int32))
+
+      init_val = (
+          jnp.zeros(shape=(), dtype=num_samples.dtype),
+          rng,
+          jnp.zeros(shape=log_prob.shape, dtype=jnp.int32),
+      )
 
       _, _, selected = jax.lax.while_loop(
           cond_fun=lambda val: jnp.less(val[0], num_samples),
           body_fun=body_fun,
-          init_val=init_val
+          init_val=init_val,
       )
       ll_selected = log_prob * selected
     else:
       perturbed_ll = gumbel(rng, loc=log_prob)
       sorted_ll = jnp.sort(perturbed_ll)
       threshold = jnp.expand_dims(
-          sorted_ll[..., num_classes - num_samples], axis=-1)
+          sorted_ll[..., num_classes - num_samples], axis=-1
+      )
       selected_mask = (perturbed_ll >= threshold).astype(jnp.int32)
       if need_ordering_info:
         selected = {
@@ -123,9 +137,11 @@ def multinomial(rng, log_prob, num_samples, replacement=True,
         idx_ll = noreplacement_sampling_renormalize(sorted_ll)
         flat_idx = jnp.reshape(sorted_idx, (-1, num_classes))
         flat_ll = jnp.reshape(idx_ll, (-1, num_classes))
-        ll_selected = jnp.zeros_like(flat_ll).at[
-            jnp.expand_dims(jnp.arange(flat_idx.shape[0]), 1), flat_idx
-        ].set(flat_ll)
+        ll_selected = (
+            jnp.zeros_like(flat_ll)
+            .at[jnp.expand_dims(jnp.arange(flat_idx.shape[0]), 1), flat_idx]
+            .set(flat_ll)
+        )
         ll_selected = jnp.reshape(ll_selected, log_prob.shape)
         ll_selected = ll_selected * selected_mask
       return selected, ll_selected
@@ -140,5 +156,11 @@ def bernoulli_logp(rng, log_prob):
 
 def mh_step(rng, log_prob, current_sample, new_sample):
   use_new_sample = bernoulli_logp(rng, log_prob)
-  return jnp.where(jnp.expand_dims(use_new_sample, range(1, new_sample.ndim)),
-                   new_sample, current_sample), use_new_sample
+  return (
+      jnp.where(
+          jnp.expand_dims(use_new_sample, range(1, new_sample.ndim)),
+          new_sample,
+          current_sample,
+      ),
+      use_new_sample,
+  )
--- discs/samplers/hammingball.py
+++ discs/samplers/hammingball.py
@@ -1,12 +1,14 @@
 """Hamming Ball Sampler Class."""
 
+import copy
+import pdb
 from discs.common import math_util as math
 from discs.samplers import abstractsampler
 import jax
 from jax import random
 import jax.numpy as jnp
 import ml_collections
-import pdb
+
 
 class HammingBallSampler(abstractsampler.AbstractSampler):
   """Random Walk Sampler Base Class."""
@@ -15,30 +17,71 @@ class HammingBallSampler(abstractsampler.AbstractSampler):
     self.num_categories = config.model.num_categories
     self.hamming = config.sampler.hamming
     self.block_size = config.sampler.block_size
-
-  def make_init_state(self, rng):
-    state = super().make_init_state(rng)
-    state['radius'] = jnp.ones(shape=(), dtype=jnp.float32)
-    pdb.set_trace()
+    assert self.hamming <= self.block_size
+    self.hamming_logit = jnp.ones(1)
     if self.num_categories == 2:
       num_samples_per_hamming = [
           math.comb(self.block_size, j + 1) for j in range(self.hamming)
       ]
-      num_samples_per_hamming = jnp.array([1] + num_samples_per_hamming)
-      self.r_init = jax.random.categorical(rng, num_samples_per_hamming)
     else:
       num_samples_per_hamming = [
           math.comb(self.block_size, j + 1)
           * (self.num_categories - 1) ** (j + 1)
           for j in range(self.hamming)
       ]
-      num_samples_per_hamming = jnp.array([1] + num_samples_per_hamming)
-      self.r_init = jax.random.categorical(rng, num_samples_per_hamming)
-   
+    self.hamming_logit += jnp.array(num_samples_per_hamming)
+
+    self.choose_index_vmapped = jax.vmap(
+        self.choose_index, in_axes=[0, None, None]
+    )
+
+  def choose_index(self, rng, arr, rad):
+    res = jax.random.choice(rng, arr, shape=(rad,), replace=False)
+    return res
+
+  def update_sampler_state(self, sampler_state):
+    sampler_state = super().update_sampler_state(sampler_state)
+    dim = math.prod(self.sample_shape)
+    sampler_state['index'] = (sampler_state['index'] + self.block_size) % dim
+    # sampler_state['num_ll_calls'] += (self.num_categories**self.block_size)
+    return sampler_state
+
+  def make_init_state(self, rng):
+    """Init sampler state."""
+    state = super().make_init_state(rng)
+    state['index'] = jnp.zeros(shape=(), dtype=jnp.int32)
     return state
 
   def step(self, model, rng, x, model_param, state, x_mask=None):
     _ = x_mask
+    x_shape = x.shape
+    x = x.reshape(x.shape[0], -1)
+    b_idx = jnp.arange(x.shape[0])
+    rad = jax.random.categorical(rng, self.hamming_logit)
+    start_index = state['index']
+    block = start_index + jnp.arange(self.block_size)
+    u = copy.deepcopy(x)
+    if rad:
+      rng_v = jax.random.split(rng, x.shape[0])
+      indices = self.choose_index_vmapped(rng_v, block, rad)
+      u[b_idx, block[indices]] = 1 - u[b_idx, block[indices]]
+
+    # Y = u.unsqueeze(1)
+    # for j in range(self.hamming):
+    #     y = torch.stack([u.clone() for _ in range(comb(self.block_size, j+1))], dim=1)
+    #     idx_pos = torch.combinations(block, j + 1)
+    #     idx_p = torch.arange(idx_pos.shape[0]).unsqueeze(-1)
+    #     y[:, idx_p, idx_pos] = 1 - y[:, idx_p, idx_pos]
+    #     Y = torch.cat([Y, y], dim=1)
+
+    # energy = model(Y)
+    # selected = torch.multinomial(torch.softmax(energy, dim=-1), 1)
+    # new_x = Y[b_idx, selected].squeeze()
+    # hop = torch.abs(new_x - x).mean(0).sum().item()
+    # self.update_stats(length=2 * self.hamming, acc=1, hop=hop)
+    # return new_x
+
+    new_state = self.update_sampler_state(state)
     return new_x, new_state, acc
 
 
