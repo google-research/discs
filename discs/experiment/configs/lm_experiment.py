1f83918b3f247cf08db57d0d3c561ddec1366461
KatayoonGoshvadi <katayoon.goshvadi+github@gmail.com>
2023-05-21 23:36:26 +0000

    running on cloud

--- discs/experiment/configs/lm_experiment.py
+++ discs/experiment/configs/lm_experiment.py
@@ -1,7 +1,7 @@
 from ml_collections import config_dict
 
 
-def get_llm_default_config():
+def get_lm_default_config():
   """Get combinatorial default configs."""
   exp_config = config_dict.ConfigDict()
   exp_config.evaluator = 'lm_eval'
