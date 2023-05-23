38e6fd02bf6b68e851925787a02aa6b3a9f18aa5
KatayoonGoshvadi <katayoon.goshvadi+github@gmail.com>
2023-05-23 03:38:21 +0000

    updated fhmm

--- discs/models/configs/fhmm_config.py
+++ discs/models/configs/fhmm_config.py
@@ -3,13 +3,11 @@ from ml_collections import config_dict
 
 def get_config():
   model_config = dict(
-      shape=(2000,),
+      shape=(100,10),
       num_categories=2,
       sigma=0.5,
       alpha=0.1,
       beta=0.8,
-      L=100,
-      K=10,
       name='fhmm',
   )
   model_config['save_dir_name'] = 'fhmm' 
--- discs/models/fhmm.py
+++ discs/models/fhmm.py
@@ -13,39 +13,40 @@ class FHMM(abstractmodel.AbstractModel):
 
   def __init__(self, config: ml_collections.ConfigDict):
     self.shape = config.shape
-    self.init_sigma = config.init_sigma
     self.num_categories = config.num_categories
-
-    self.l = config.L
-    self.k = config.K
+    self.l = self.shape[0]
+    self.k = self.shape[1]
     self.sigma = config.sigma
-    self.p_x0 = functools.partial(jax.random.bernoulli, p=self.alpha)
-    self.p_xc = functools.partial(jax.random.bernoulli, p=1 - self.beta)
 
   def make_init_params(self, rnd):
-    rng1, rng2, rng3, rng4 = jax.random.split(rnd, 4)
+    rng1, rng2, rng3, rng4, rng5 = jax.random.split(rnd, 5)
     x = self.sample_X(rng1)
-    w = jax.random.normal(rng2, (self.k, 1))
-    b = jax.random.normal(rng3, (1, 1))
+    self.w = jax.random.normal(rng2, (self.k, 1))
+    self.b = jax.random.normal(rng3, (1, 1))
+    self.y = self.sample_Y(rng4, x)
     params = {}
-    params['params'] = {}
-    params['params']['Y'] = self.sample_Y(rng4, x, w, b)
-    params['params']['W'] = w
-    params['params']['b'] = b
-    params['params']['rng'] = rng4
+    params['params'] = rng5
     return params
 
   def sample_X(self, rng):
     x = jnp.ones([self.l, self.k])
-    x[0] = jax.random.bernoulli(rng, p=x[0] * self.alpha)
+    x = x.at[0].set(jax.random.bernoulli(rng, p=x[0] * self.alpha))
     for l in range(1, self.l):
       rng, _ = jax.random.split(rng)
       p = self.beta * x[l - 1] + (1 - self.beta) * (1 - x[l - 1])
-      x[l] = jax.random.bernoulli(rng, p)
+      x = x.at[l].set(jax.random.bernoulli(rng, p))
     return x
 
-  def sample_Y(self, rng, x, w, b):
-    return jax.random.normal(rng, (self.l, 1)) * self.sigma + x @ w + b
+  def sample_Y(self, rng, x):
+    return jax.random.normal(rng, (self.l, 1)) * self.sigma + x @ self.w + self.b
+  
+  def log_probab_of_px(self, x, p):
+    num_ones = jnp.sum(x)
+    dim = math.prod(x.shape)
+    prob = (p**(num_ones))*( (1-p)**(dim-num_ones))
+    return jnp.log(prob)
+    
+  def log_probab_of_p_xc(self, x):
 
   def get_init_samples(self, rng, num_samples: int):
     x0 = jax.random.bernoulli(
@@ -53,17 +54,6 @@ class FHMM(abstractmodel.AbstractModel):
         shape=(num_samples,) + (self.l, self.k),
     )
     return x0
-
-  # def get_init_samples(self, rnd, num_samples: int):
-  #   x0 = jax.random.randint(
-  #       rnd,
-  #       shape=(num_samples,) + self.shape,
-  #       minval=0,
-  #       maxval=self.num_categories,
-  #       dtype=jnp.int32,
-  #   )
-
-  #   return x0
 
   def forward(self, params, x):
     params = params['params']
@@ -73,8 +63,8 @@ class FHMM(abstractmodel.AbstractModel):
     x_cur = x[:, :-1, :]
     x_next = x[:, 1:, :]
     x_c = x_cur * (1 - x_next) + (1 - x_cur) * x_next
-    logp_x = jnp.sum(jnp.log(self.p_x0(rng1, x_0)), -1) + jnp.sum(
-        jnp.log(self.p_xc(rng2, x_c)), [-1, -2]
+    logp_x = jnp.sum(self.log_probab_of_px(x_0, self.alpha), -1) + jnp.sum(
+        jnp.log(self.log_probab_of_px(x_c, 1 - self.beta)), [-1, -2]
     )
     logp_y = -jnp.sum(
         jnp.square(params['Y'] - x @ params['W'] - params['b']), [-1, -2]
