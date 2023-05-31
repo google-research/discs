e58c0815093534363a80c66d21942ae034b80b5f
KatayoonGoshvadi <katayoon.goshvadi+github@gmail.com>
2023-05-31 19:15:28 +0000

    updated original hammingball to more memmory efficient one

--- discs/samplers/configs/hammingball_update_config.py
+++
@@ -1,9 +0,0 @@
-from ml_collections import config_dict
-
-
-def get_config():
-  model_config = dict(
-      name='hammingball_update',
-      block_size=10,
-  )
-  return config_dict.ConfigDict(model_config)
