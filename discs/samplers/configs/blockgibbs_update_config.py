647f52e5de01c487ef3c59076ca609591a280801
KatayoonGoshvadi <katayoon.goshvadi+github@gmail.com>
2023-05-31 20:49:55 +0000

    original blockgibbs updated with a more efficient variant

--- discs/samplers/configs/blockgibbs_update_config.py
+++
@@ -1,9 +0,0 @@
-from ml_collections import config_dict
-
-
-def get_config():
-  model_config = dict (
-      block_size=2,
-      name='blockgibbs_update',
-  )
-  return config_dict.ConfigDict(model_config)
